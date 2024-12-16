da3eeebccd2f43e1c27ac816c66a9b406a7275aa
888Saint <psdas32@gmail.com>
2024-12-16 18:21:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstWord(s) {
-  // your code here
+  // Trim leading spaces to ignore them when searching for the first word
+  s = s.trim();
+
+  // Find the index of the first space
+  const spaceIndex = s.indexOf(" ");
+
+  // If no space is found, return the entire string
+  if (spaceIndex === -1) {
+    return s;
+  }
+
+  // Return the substring from the start to the first space
+  return s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
